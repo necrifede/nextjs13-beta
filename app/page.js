9e1ff1deb6c7643f53034131aa93228d7ac76a4b
Nestor Felipe <necrifede@gmail.com>
2023-03-06 11:00:47 +0100

    Adding 'hola mundo vercel' message

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
+      Hola Mundo Vercel!
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
